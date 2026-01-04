60d24f7abbb0a0514b2f288150b8cfd5f5090fe6
omar <omar.i.s.diab@gmail.com>
2026-01-05 01:01:09 +0300

    Fixing code

--- server/DB/dbcon.js
+++ server/DB/dbcon.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,             
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,               // 143.110.246.243
+  port: Number(process.env.DB_PORT),        // 28799
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
 
   ssl: {
     ca: process.env.DB_SSL_CA,
-    servername: process.env.DB_SSL_SERVERNAME, 
+    servername: process.env.DB_SSL_SERVERNAME, // bookstore-omardiab.l.aivencloud.com
   },
 });
 
