aa5ac0ae6a66a0a56e03a1be5e6ca5baee9c0bab
omar <omar.i.s.diab@gmail.com>
2026-01-05 01:06:23 +0300

    Fixing code

--- server/DB/dbcon.js
+++ server/DB/dbcon.js
@@ -1,8 +1,8 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,               // 143.110.246.243
-  port: Number(process.env.DB_PORT),        // 28799
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -12,16 +12,8 @@ const pool = mysql.createPool({
 
   ssl: {
     ca: process.env.DB_SSL_CA,
-    servername: process.env.DB_SSL_SERVERNAME, // bookstore-omardiab.l.aivencloud.com
+    servername: process.env.DB_SSL_SERVERNAME,
   },
 });
 
-module.exports = pool;
-
-
-// Update the export to include the pool
-export default {
-    query: (sql, params) => pool.query(sql, params),
-    getConnection: () => pool.getConnection(), // Add this line
-    pool: pool // Export the pool directly just in case
-};
+export default pool;
